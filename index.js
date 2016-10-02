316ed862b87d883e50c72ee6c12a0ebdaf2683d3
Thomas Watson Steen <w@tson.dk>
2016-10-02 17:47:15 +0200

    standard: fix all outstanding linter issues

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
-'use strict';
+'use strict'
 
-var murl = require('murl');
+var murl = require('murl')
 
 var Patterns = module.exports = function (patterns) {
-  if (!(this instanceof Patterns))
-    return new Patterns(patterns);
+  if (!(this instanceof Patterns)) return new Patterns(patterns)
+
   this._patterns = (patterns || []).map(function (ptn) {
-    return [compile(ptn), ptn, undefined];
-  });
-};
+    return [compile(ptn), ptn, undefined]
+  })
+}
 
 var compile = function (ptn) {
-  if (typeof ptn === 'string') return murl(ptn);
+  if (typeof ptn === 'string') return murl(ptn)
   return function (target) {
-    return target.match(ptn);
-  };
-};
+    return target.match(ptn)
+  }
+}
 
 Patterns.prototype.add = function (ptn, val) {
-  this._patterns.push([compile(ptn), ptn, val]);
-};
+  this._patterns.push([compile(ptn), ptn, val])
+}
 
 Patterns.prototype.match = function (target, index) {
-  var self = this;
+  var self = this
   var next = function () {
-    return self.match(target, index+1);
-  };
-  var ptn, match;
-  if (!index) index = 0;
+    return self.match(target, index + 1)
+  }
+  var ptn, match
+  if (!index) index = 0
   for (var l = this._patterns.length; index < l; index++) {
-    ptn = this._patterns[index];
-    match = ptn[0](target);
-    if (match) return {
-      target: target,
-      pattern: ptn[1],
-      params: match,
-      value: ptn[2],
-      next: next
-    };
+    ptn = this._patterns[index]
+    match = ptn[0](target)
+    if (match) {
+      return {
+        target: target,
+        pattern: ptn[1],
+        params: match,
+        value: ptn[2],
+        next: next
+      }
+    }
   }
-  return null;
-};
+  return null
+}
 
-Patterns.prototype.matchAll = function(target) {
-  var matches = [];
-  var ptn, match;
+Patterns.prototype.matchAll = function (target) {
+  var matches = []
+  var ptn, match
   for (var i = 0; i < this._patterns.length; i++) {
-     ptn = this._patterns[i]
-     match = ptn[0](target);
-     if (match) matches.push({
-       target: target,
-       pattern: ptn[1],
-       params: match,
-       value: ptn[2]
-     });
+    ptn = this._patterns[i]
+    match = ptn[0](target)
+    if (match) {
+      matches.push({
+        target: target,
+        pattern: ptn[1],
+        params: match,
+        value: ptn[2]
+      })
+    }
   }
 
-  return matches;
-};
+  return matches
+}
--- test.js
+++ test.js
@@ -1,88 +1,87 @@
-'use strict';
+'use strict'
 
-var test = require('tape');
-var murl = require('murl');
-var Patterns = require('./');
+var test = require('tape')
+var murl = require('murl')
+var Patterns = require('./')
 
 test('no patterns', function (t) {
-  var p = Patterns();
-  var match = p.match('foo');
-  t.equal(match, null);
-  t.end();
-});
+  var p = Patterns()
+  var match = p.match('foo')
+  t.equal(match, null)
+  t.end()
+})
 
 test('string patterns', function (t) {
-  var p = Patterns();
-  p.add('foo', 1);
-  p.add('bar', 2);
-  p.add('baz', 3);
-  var match = p.match('bar');
-  t.equal(match.target, 'bar');
-  t.equal(match.pattern, 'bar');
-  t.deepEqual(match.params, murl('bar')('bar'));
-  t.equal(match.value, 2);
-  t.ok(typeof match.next, 'function');
-  t.end();
-});
+  var p = Patterns()
+  p.add('foo', 1)
+  p.add('bar', 2)
+  p.add('baz', 3)
+  var match = p.match('bar')
+  t.equal(match.target, 'bar')
+  t.equal(match.pattern, 'bar')
+  t.deepEqual(match.params, murl('bar')('bar'))
+  t.equal(match.value, 2)
+  t.ok(typeof match.next, 'function')
+  t.end()
+})
 
 test('murl patterns', function (t) {
-  var p = Patterns();
-  p.add('/{hello}/{world}?');
-  var match = p.match('/foo/bar');
-  t.deepEqual(match.params, { hello: 'foo', world: 'bar' });
-  match = p.match('/foo');
-  t.deepEqual(match.params, { hello: 'foo', world: undefined });
-  t.end();
-});
+  var p = Patterns()
+  p.add('/{hello}/{world}?')
+  var match = p.match('/foo/bar')
+  t.deepEqual(match.params, { hello: 'foo', world: 'bar' })
+  match = p.match('/foo')
+  t.deepEqual(match.params, { hello: 'foo', world: undefined })
+  t.end()
+})
 
 test('RegExp patterns', function (t) {
-  var p = Patterns();
-  p.add(/foo/, 1);
-  p.add(/bar/, 2);
-  p.add(/baz/, 3);
-  var match = p.match('bar');
-  t.equal(match.target, 'bar');
-  t.deepEqual(match.pattern, /bar/);
-  t.deepEqual(match.params, 'bar'.match(/bar/));
-  t.equal(match.value, 2);
-  t.ok(typeof match.next, 'function');
-  t.end();
-});
+  var p = Patterns()
+  p.add(/foo/, 1)
+  p.add(/bar/, 2)
+  p.add(/baz/, 3)
+  var match = p.match('bar')
+  t.equal(match.target, 'bar')
+  t.deepEqual(match.pattern, /bar/)
+  t.deepEqual(match.params, 'bar'.match(/bar/))
+  t.equal(match.value, 2)
+  t.ok(typeof match.next, 'function')
+  t.end()
+})
 
 test('next function', function (t) {
-  var p = Patterns();
-  p.add(/foo/, 1);
-  p.add(/bar/, 2);
-  p.add(/baz/, 3);
-  var match = p.match('foobar');
-  t.equal(match.value, 1);
-  match = match.next();
-  t.equal(match.value, 2);
-  match = match.next();
-  t.equal(match, null);
-  t.end();
-});
+  var p = Patterns()
+  p.add(/foo/, 1)
+  p.add(/bar/, 2)
+  p.add(/baz/, 3)
+  var match = p.match('foobar')
+  t.equal(match.value, 1)
+  match = match.next()
+  t.equal(match.value, 2)
+  match = match.next()
+  t.equal(match, null)
+  t.end()
+})
 
 test('last next function', function (t) {
-  var p = Patterns();
-  p.add(/foo/, 1);
-  var match = p.match('foo');
-  t.equal(match.value, 1);
-  match = match.next();
-  t.equal(match, null);
-  t.end();
-});
+  var p = Patterns()
+  p.add(/foo/, 1)
+  var match = p.match('foo')
+  t.equal(match.value, 1)
+  match = match.next()
+  t.equal(match, null)
+  t.end()
+})
 
 test('matchAll', function (t) {
-  var p = Patterns();
-  p.add(/foo/, 1);
-  p.add(/foo/, 2);
-  p.add(/foo/, 3);
-  var matches = p.matchAll('foo');
-  t.equal(matches[0].value, 1);
-  t.equal(matches[1].value, 2);
-  t.equal(matches[2].value, 3);
-  t.end();
-});
-
+  var p = Patterns()
+  p.add(/foo/, 1)
+  p.add(/foo/, 2)
+  p.add(/foo/, 3)
+  var matches = p.matchAll('foo')
+  t.equal(matches[0].value, 1)
+  t.equal(matches[1].value, 2)
+  t.equal(matches[2].value, 3)
+  t.end()
+})
 
